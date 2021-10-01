2400029c1130a6be38411e7aaa831b3040af0878
ibrahimdevoloper <ibrahimdevoloper@gmail.com>
2021-10-01 08:21:10 +0300

    Initial commit

---
+++ database/migrations/2021_10_01_044648_create_inventories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            $table->timestamp('date');
+            $table->string('details');
+            $table->string('code',6);
+            $table->string('delivery_name')->nullable();
+            $table->string('delivery_contact')->nullable();
+            $table->string('notes')->nullable();
+            $table->integer('expense')->nullable();
+            $table->foreignId('products')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
---
+++ database/migrations/2021_10_01_045706_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total_price');
+            $table->timestamp('date');
+            $table->string('customer_notes')->nullable();
+            $table->string('admin_notes')->nullable();
+            $table->string('code',12);
+            $table->string('delivery_name')->nullable();
+            $table->string('delivery_contact')->nullable();
+            $table->boolean('is_delivered')->default(false);
+            $table->boolean('is_varified')->default(false);
+            $table->foreignId('users')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
